1d9e04705ba52d33521eb615b9768ac07a4175d6
Pedro <pedro.collares05@gmail.com>
2024-08-16 16:20:52 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import calendar
+
+yy = 2024
+mm = 12
+
+print(calendar.month(yy, mm))
+
+
+if(yy == 12):
+    print("vsf")
+    if(mm == 12):
+        print("vsf denovo")
+
+
+
+def vsf():
+    i = 1
+    for i in range(10):
+        print("vsf")
+        
+        
+        
+vsf()
+    
